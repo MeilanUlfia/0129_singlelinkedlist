08357fe12172cd01edcb73d2a5027f2a1d18b8ba
meilan <meilanulfia@gmail.com>
2025-05-08 16:48:56 +0700

    menambah menu dengan opsi tambah, hapus, tampilkan, cari, dan keluar

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -112,3 +112,24 @@ void traverse()
 }
 };
 
+int main()
+{
+    List mhs;
+    int nim;
+    char ch;
+    while(1)
+    {
+        cout
+             << "Menu" << endl;
+        cout << endl
+             << "1. Menambah data kedalam list" << endl;
+        cout << "2. Mengahpus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout
+             << "Masukkan pilihan (1-5): " << endl;
+        cin >> ch;
+    }
+}
+   
