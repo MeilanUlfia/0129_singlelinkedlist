2742804ccc2297364781494d179214c40edf21c2
meilan <meilanulfia@gmail.com>
2025-05-08 16:58:08 +0700

    implementasikan case 4 untuk mencari data berdasarkan NIM dalam linked list

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -163,6 +163,15 @@ int main()
             mhs.traverse();
         }
         break;
+
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList Kosong\n";
+                break;
+            }
+        }
     }
 }
    
